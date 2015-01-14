0ae551fb5c39a1ec9f00577b5015c5ea4bc50a02
Hoang Nguyen <contact@hoangnd.me>
2015-01-15 02:19:50 +1100

    Add python support for PBCKCODE

--- src/App/MainBundle/Resources/public/js/ckeditor/config.js
+++ src/App/MainBundle/Resources/public/js/ckeditor/config.js
@@ -7,4 +7,24 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	
+	// PBCKCODE CUSTOMIZATION
+	     config.pbckcode = {
+	         // An optional class to your pre tag.
+	         cls : '',
+
+	         // The syntax highlighter you will use in the output view
+	         highlighter : 'PRETTIFY',
+
+	         // An array of the available modes for you plugin.
+	         // The key corresponds to the string shown in the select tag.
+	         // The value correspond to the loaded file for ACE Editor.
+	         modes :  [ ['HTML', 'html'], ['CSS', 'css'], ['PHP', 'php'], ['JS', 'javascript'], ['Python', 'python'] ],
+
+	         // The theme of the ACE Editor of the plugin.
+	         theme : 'textmate',
+
+	         // Tab indentation (in spaces)
+	         tab_size : '4'
+	     };
 };
